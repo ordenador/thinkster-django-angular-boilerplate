f950850d8ad741f2e89fdd8055a90c0f79e50393
ordenador <mariofaundez@hotmail.com>
2014-12-02 00:52:17 -0300

    Guide at 'Registering new users, but problems with: rest_framework_nested

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -2,8 +2,17 @@ from django.conf.urls import patterns, url
 
 from thinkster_django_angular_boilerplate.views import IndexView
 
+from rest_framework_nested import routers
+
+from authentication.views import AccountViewSet
+
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
+
 urlpatterns = patterns(
     '',
-
     url('^.*$', IndexView.as_view(), name='index'),
+    url(r'^api/v1/', include(router.urls)),
+    url(r'^', TemplateView.as_view(template_name='index.html')),
 )
